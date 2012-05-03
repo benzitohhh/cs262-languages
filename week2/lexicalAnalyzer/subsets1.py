d3bf927c7af64c3ed5782d350c79c6743e0a0cb4
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2012-05-03 19:34:36 -0300

    some week3 stuff

---
+++ week2/lexicalAnalyzer/subsets1.py
@@ -0,0 +1,23 @@
+# Bonus Practice: Subsets
+
+# This assignment is not graded and we encourage you to experiment. Learning is
+# fun!
+
+# Write a procedure that accepts a list as an argument. The procedure should
+# print out all of the subsets of that list.
+
+def all_sublists(big_list):
+	sublists(big_list, [])
+
+def sublists(big_list, selected_so_far):
+	# base case
+	if big_list == []:
+		print selected_so_far
+	else:
+		 current_element = big_list[0]
+		 rest_of_big_list = big_list[1:]
+		 sublists(rest_of_big_list, selected_so_far + [current_element])
+		 sublists(rest_of_big_list, selected_so_far)
+
+dinner_guests = ["a", "b", "c"]		 
+all_sublists(dinner_guests)
